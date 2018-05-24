060005c66cb7df4ad03e3cc52e2856ca919b44bc
ashwiniagre1 <ashwiniagre1>
2018-05-24 13:56:28 +0000

    user profile added

---
+++ src/app/pages/demoappbc/test/userregistration/userregistration.component.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by: MetaMagic
+ * Date: 24/05/2018
+ * Organization: MetaMagic	
+ */
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'userregistration',
+  templateUrl: 'userregistration.component.html'
+})
+export class UserregistrationComponent implements OnInit{
+  userregistrationModel:UserregistrationModel;
+        radiogroupData:any; 
+  constructor(private http: HttpClient) {
+    this.userregistrationModel=new UserregistrationModel();
+     	 	this.radiogroupData={"data":[{"gender":"female","genderId":"key"},{"gender":"male","genderId":"key"}]};
+	  }
+        onBlur_firstname(eventData:any){} 
+      input_firstname(eventData:any){} 
+      focus_firstname(eventData:any){} 
+        onBlur_lastname(eventData:any){} 
+      input_lastname(eventData:any){} 
+      focus_lastname(eventData:any){} 
+        onBlur_phonenumber(eventData:any){} 
+      input_phonenumber(eventData:any){} 
+      focus_phonenumber(eventData:any){} 
+        onSelection_gender(eventData:any){} 
+    ngOnInit(){
+ 	  }
+ 	 	
+
+   
+}
+export class UserregistrationModel{
+  firstname: string;  lastname: string;  phonenumber: number;  gender: string; }
